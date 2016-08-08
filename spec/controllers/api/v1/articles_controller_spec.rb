0cae232001551e7c6711278cea6f32352d019c19
irenliber <irr.morgo@gmail.com>
2016-08-08 12:58:50 +0600

    added specs for show, update, destroy

--- spec/controllers/api/v1/articles_controller_spec.rb
+++ spec/controllers/api/v1/articles_controller_spec.rb
@@ -37,4 +37,43 @@ RSpec.describe Api::V1::ArticlesController, type: :controller do
       end
     end
   end
+
+  describe 'GET show' do
+
+    before(:each) {@article = create(:article)}
+    it "should assign @article to the article" do
+      get :show, id: @article.id
+      expect(response.status).to eq 200
+      assigns(:article).id.should eq(@article.id)
+    end
+  end
+
+  describe 'PUT update' do
+    before(:each) {@article = create(:article)}
+
+    let(:attr) do
+      { :title => 'new title', :body => 'new content' }
+    end
+
+    before(:each) do
+      put :update, :id => @article.id, :article => attr
+      @article.reload
+    end
+
+    it 'status is ok' do
+          subject
+          expect(response.status).to eq 204
+        end
+    it { expect(@article.title).to eql attr[:title] }
+    it { expect(@article.body).to eql attr[:body] }
+  end
+
+  describe 'DELETE destroy' do
+    before(:each) {@article = create(:article)}
+    it "destroys the requested article" do
+      expect {delete :destroy, id: @article.id}.to change(Article, :count).by(-1)
+      expect(response.status).to eq 204
+      end
+  end
+
 end
